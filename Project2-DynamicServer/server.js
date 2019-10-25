a73a2cbfb0a3f9dab758c697597dd69f81b32f7f
Leo Molitor <43321346+moli1133@users.noreply.github.com>
2019-10-24 19:28:22 -0500

    GET handler for state page done
    State page should work now. I didnt use promises so it's a little bit gross with the nested calbacks but it works.

--- Project2-DynamicServer/server.js
+++ Project2-DynamicServer/server.js
@@ -40,11 +40,79 @@ app.get('/', (req, res) => {
 });
 
 // GET request handler for '/year/*'
-app.get('/year/:selected_year', (req, res) => {
-    ReadFile(path.join(template_dir, 'year.html')).then((template) => {
-        let response = template;
-        // modify `response` here
-        WriteHtml(res, response);
+app.get('/year/:selected_year', (req, res) => {	
+    ReadFile(path.join(template_dir, 'year.html')).then((template) => {		
+		let sql = "SELECT * FROM Consumption WHERE year="+req.params.selected_year;
+		let coal_count=0;
+		let natural_gas_count=0;
+		let nuclear_count=0;
+		let petroleum_count=0;
+		let renewable_count=0;
+		db.all(sql, [], (err, rows) => {
+			if (err) {
+				throw err;
+			}
+			//Length of query, should be 51 for the 50 states + Washington DC
+			let length = Object.keys(rows).length;
+			console.log("LENGTH: " + length);
+			//Table data that will be inserted in consumption by state table
+			let table_data="";
+			for(i = 0; i < length; i++){
+				//Start new row in table
+				table_data=table_data+"<tr>";
+				
+				//1st column in table is the state 
+				table_data=table_data+"<td>"+rows[i].state_abbreviation+"</td>";
+				
+				//Update total coal amount for pie chart
+				coal_count = coal_count+rows[i].coal;
+				//2nd column in table is coal
+				table_data=table_data+"<td>"+rows[i].coal+"</td>";
+				
+				//Update total natural gas amount for pie chart
+				natural_gas_count = natural_gas_count+rows[i].natural_gas;
+				//3rd column in table is natural gas
+				table_data=table_data+"<td>"+rows[i].natural_gas+"</td>";
+				
+				//Update total nuclear amount for pie chart
+				nuclear_count = nuclear_count+rows[i].nuclear;
+				//4th column in table is nuclear
+				table_data=table_data+"<td>"+rows[i].nuclear+"</td>";
+				
+				//Update total petroleum amount for pie chart
+				petroleum_count = petroleum_count+rows[i].petroleum;
+				//5th column in table is petroleum
+				table_data=table_data+"<td>"+rows[i].petroleum+"</td>";
+				
+				//Update total renewable amount for pie chart
+				renewable_count = renewable_count+rows[i].renewable;
+				//6th column in table is renewable
+				table_data=table_data+"<td>"+rows[i].renewable+"</td>";
+				
+				//7th (last) column in table is total energy usage
+				let total=(rows[i].coal + rows[i].natural_gas + rows[i].nuclear + rows[i].petroleum + rows[i].renewable);
+				table_data=table_data+"<td>"+total+"</td>";
+				
+				//End this table row
+				table_data=table_data+"</tr>";
+			}			
+
+			let response = template.toString();
+			// modify `response` here
+			// modify title
+			response=response.replace("<title>US Energy Consumption</title>", req.params.selected_year+" US Energy Consumption");
+			// modify variables
+			response=response.replace("var year", "var year="+req.params.selected_year);
+			response=response.replace("var coal_count", "var coal_count="+coal_count);
+			response=response.replace("var natural_gas_count", "var natural_gas_count="+natural_gas_count);
+			response=response.replace("var nuclear_count", "var nuclear_count="+nuclear_count);
+			response=response.replace("var petroleum_count", "var petroleum_count="+petroleum_count);
+			response=response.replace("var renewable_count", "var renewable_count="+renewable_count);
+			//modify table
+			response=response.replace("<!-- Data to be inserted here -->", table_data);
+			
+			WriteHtml(res, response);
+		});
     }).catch((err) => {
         Write404Error(res);
     });
@@ -52,6 +120,7 @@ app.get('/year/:selected_year', (req, res) => {
 
 // GET request handler for '/state/*'
 app.get('/state/:selected_state', (req, res) => {
+	console.log("SELECTED STATE: " + req.params.selected_state);
     ReadFile(path.join(template_dir, 'state.html')).then((template) => {
         let response = template;
         // modify `response` here
@@ -63,6 +132,7 @@ app.get('/state/:selected_state', (req, res) => {
 
 // GET request handler for '/energy-type/*'
 app.get('/energy-type/:selected_energy_type', (req, res) => {
+	console.log("SELECTED ENERGY TYPE: " + req.params.selected_energy_type);
     ReadFile(path.join(template_dir, 'energy.html')).then((template) => {
         let response = template;
         // modify `response` here
